7ff8b9785afab83daaf340f2bcca60c5616ca274
daviaquino87 <davi26031@gmail.com>
2023-03-10 09:32:23 -0300

    test: Criado test de integracao

---
+++ search_cep/src/modules/cep/useCases/searchCep/SearchCepController.spec.ts
@@ -0,0 +1,13 @@
+import { app } from "../../../../shared/infra/http/app";
+import request from "supertest";
+
+describe("Search CEP controller", () => {
+  it("should be able possible to search a CEP", async () => {
+    const { body } = await request(app)
+      .get("/cep/search")
+      .query({ cep: "63460000" })
+      .expect(200);
+
+    expect(body.city).toBe("Pereiro");
+  });
+});
--- search_cep/src/shared/infra/http/app.ts
+++ search_cep/src/shared/infra/http/app.ts
@@ -3,9 +3,11 @@ import express from "express";
 import { verifyError } from "./middlewares/verifyError";
 import { indexRoutes } from "./routes/index.routes";
 
-export const app = express();
+const app = express();
 
 app.use(express.json());
 app.use(indexRoutes);
 
 app.use(verifyError);
+
+export { app };
